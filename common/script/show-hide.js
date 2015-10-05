30443e0a89ae0f9f33751ad111d84f9d11c5d28e
arrowrowe <arrowrowe@gmail.com>
2015-10-05 13:05:50 +0800

    Add show-hide

---
+++ common/script/show-hide.js
@@ -0,0 +1,7 @@
+(function () {
+  'use strict';
+  $('*[data-show-hide]').click(function () {
+    var $target = $($(this).data('show-hide'));
+    $target[$target.is(':visible') ? 'hide' : 'show']();
+  });
+})();
