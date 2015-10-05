e6f0feaa9eac6db7a1a89e450dbff8b663fe2445
arrowrowe <arrowrowe@gmail.com>
2015-10-05 14:32:57 +0800

    Remove global jquery in eslint

--- common/script/show-hide.js
+++ common/script/show-hide.js
@@ -1,7 +1,7 @@
-(function () {
+(function ($) {
   'use strict';
   $('*[data-show-hide]').click(function () {
     var $target = $($(this).data('show-hide'));
     $target[$target.is(':visible') ? 'hide' : 'show']();
   });
-})();
+})(window.$);
